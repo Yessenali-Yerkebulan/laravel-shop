6bca2d87e47e8e00fa58446d3e4adc115b9b44ea
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-06-13 17:24:01 +0100

    initial project, add routes index page, categories, single product

---
+++ resources/views/index.blade.php
@@ -0,0 +1,201 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Интернет Магазин</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/starter-template.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Интернет Магазин</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li  class="active" ><a href="#">Все товары</a></li>
+                <li ><a href="#">Категории</a>
+                </li>
+                <li ><a href="#">В корзину</a></li>
+                <li><a href="#">Сбросить проект в начальное состояние</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Панель администратора</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<div class="container">
+    <div class="starter-template">
+        <h1>Все товары</h1>
+
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="https://avatars.mds.yandex.net/i?id=4a4971079fdf3781b41e471455ccb807af141ae6-9271342-images-thumbs&n=13" alt="iPhone X 64GB">
+                    <div class="caption">
+                        <h3>iPhone X 64GB</h3>
+                        <p>71990 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="https://shop60515.sfstatic.io/upload_dir/shop/Apple-iPhone-X-Silver.w1200.jpg" alt="iPhone X 256GB">
+                    <div class="caption">
+                        <h3>iPhone X 256GB</h3>
+                        <p>89990 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="http://mobile-plus.kz/image/cache/data/products/1574-500x500.jpg" alt="HTC One S">
+                    <div class="caption">
+                        <h3>HTC One S</h3>
+                        <p>12490 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="https://avatars.mds.yandex.net/i?id=0aa722cc46662a4b7d0b313220d0d3a1a2ec5c4f-4033784-images-thumbs&n=13" alt="iPhone 5SE">
+                    <div class="caption">
+                        <h3>iPhone 5SE</h3>
+                        <p>17221 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MH6P2?wid=1144&hei=1144&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=0" alt="Наушники Beats Audio">
+                    <div class="caption">
+                        <h3>Наушники Beats Audio</h3>
+                        <p>20221 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="https://avatars.mds.yandex.net/i?id=2bcdd1e378ec6e6057442be610dc3ce9d710ccdf-8454246-images-thumbs&n=13" alt="Камера GoPro">
+                    <div class="caption">
+                        <h3>Камера GoPro</h3>
+                        <p>12000 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="https://m.media-amazon.com/images/I/71QDBzbzrxL._AC_SL1500_.jpg" alt="Камера Panasonic HC-V770">
+                    <div class="caption">
+                        <h3>Камера Panasonic HC-V770</h3>
+                        <p>27900 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="https://www.tradeinn.com/f/13785/137859222/delonghi-ecam-21.117-b-Кофеварка-Эспрессо.jpg" alt="Кофемашина DeLongi">
+                    <div class="caption">
+                        <h3>Кофемашина DeLongi</h3>
+                        <p>25200 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="https://i.ebayimg.com/00/s/MTYwMFgxNjAw/z/6ZoAAOSwQX1fBzq9/$_57.JPG?set_id=8800005007" alt="Холодильник Haier">
+                    <div class="caption">
+                        <h3>Холодильник Haier</h3>
+                        <p>40200 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="https://www.lostelecom.ru/image/cache/catalog/blendery-folder/3/746089982-blender-statsionarnyj-moulinex-lm439d10-1000x1000.jpg" alt="Блендер Moulinex">
+                    <div class="caption">
+                        <h3>Блендер Moulinex</h3>
+                        <p>4200 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="https://media3.bsh-group.com/Product_Shots/3000x3000/MCSA00666855_E3887_MFW45020_318951_def.jpg" alt="Мясорубка Bosch">
+                    <div class="caption">
+                        <h3>Мясорубка Bosch</h3>
+                        <p>9200 руб.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary" role="button">В корзину</a>
+                            <a href="#" class="btn btn-default"
+                               role="button">Подробнее</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
